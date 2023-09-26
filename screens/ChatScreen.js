060dfbb8988c0c7343542cdc39510f9cd2b86feb
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-09-27 04:44:05 +0530

    Chat Screen Completed

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,16 +1,55 @@
-import React, { useState } from "react";
+import React, { useCallback, useLayoutEffect, useState } from "react";
 import { View } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
+import { auth, db } from "../firebase";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
 
 export default function ChatScreen() {
   const [messages, setMessages] = useState([]);
 
-  // Function to handle new messages
-  const handleSend = (newMessages = []) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, newMessages)
+  useLayoutEffect(() => {
+    const collectionRef = collection(db, "chats");
+    const q = query(
+      collectionRef,
+      (ref) => ref.orderBy("createdAt", "desc") // Order messages by createdAt in descending order
     );
-  };
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setMessages(
+        snapshot.docs.map((doc) => ({
+          _id: doc.id,
+          createdAt: doc.data().createdAt?.toDate(),
+          text: doc.data().text,
+          user: doc.data().user,
+        }))
+      );
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  // Function to handle new messages
+  const handleSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+
+    const { _id, text, user } = messages[0];
+    const createdAt = serverTimestamp();
+
+    addDoc(collection(db, "chats"), {
+      _id,
+      createdAt,
+      text,
+      user,
+    });
+  }, []);
 
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
@@ -18,8 +57,8 @@ export default function ChatScreen() {
         messages={messages}
         onSend={(newMessages) => handleSend(newMessages)}
         user={{
-          _id: 1, // Replace with the user's unique ID
-          name: "John", // Replace with the user's name
+          _id: auth?.currentUser?.uid,
+          name: auth?.currentUser?.email,
         }}
       />
     </View>
